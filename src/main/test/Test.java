479bc439d300f23d9bcfc05e511ff70b32c8c0ad
lipeng <718118619@qq.com>
2017-09-24 19:22:13 +0800

    name: honddy/photomanage/application/ApplicationControler.java
    jersey framwork

---
+++ src/main/test/Test.java
@@ -0,0 +1,10 @@
+import org.mybatis.spring.SqlSessionFactoryBean;
+
+public class Test {
+
+    public static void main(String[] args)
+    {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+
+    }
+}
